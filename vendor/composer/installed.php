aeef810575f7b3b4a7a6349501d4c9fe63d61f39
J Hunt <github.com@huntdesign.co.nz>
2023-06-13 03:08:48 +0000

    #3 Adjust info.yml and add query access check for Drupal 10

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd3211283d738c4eba4604ad6da839ee246731705',
+    'name' => 'mjordan/redirect_from_identifier',
+  ),
+  'versions' => 
+  array (
+    'mjordan/redirect_from_identifier' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd3211283d738c4eba4604ad6da839ee246731705',
+    ),
+  ),
+);
